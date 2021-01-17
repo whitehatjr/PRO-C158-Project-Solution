7593a13f1a38624736ba70c66aec3c1f1c2af86a
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-01-17 18:55:21 +0530

    Add files via upload

--- js/highlights.js
+++ js/highlights.js
@@ -5,22 +5,7 @@ AFRAME.registerComponent("cursor-listener", {
   init: function () {
     this.handleMouseEnterEvents();
     this.handleMouseLeaveEvents();
-  },
-  update: function () {
-    const fadeBackgroundEl = document.querySelector("#fadeBackground");
-
-    //check if the infoBanner plane already has comic text info child entity
-    //if so remove the child to avoid the overlapping of the text
-    c = fadeBackgroundEl.children;
-    if (c.length > 0) {
-      var i;
-      for (i = 0; i <= c.length; i++) {
-        fadeBackgroundEl.removeChild(c[i]);
-      }
-    } else {
-      this.handleMouseClickEvents();
-    }
-  },
+  },  
   handleMouseEnterEvents: function () {
     // Mouse Enter Events
     this.el.addEventListener("mouseenter", () => {
@@ -53,37 +38,5 @@ AFRAME.registerComponent("cursor-listener", {
       }
     });
   },
-  handleMouseClickEvents: function () {
-    // Mouse Click Events
-    this.el.addEventListener("click", () => {
-      const { selectedItemId } = this.data;
-
-      const fadeBackgroundEl = document.querySelector("#fadeBackground");
-      const titleEl = document.querySelector("#app-title");
-      const cursorEl = document.querySelector("#camera-cursor");
-
-      //check the selected item to set the "info-banner" component on the plane
-      if (selectedItemId) {
-        fadeBackgroundEl.setAttribute("visible", true);
-        fadeBackgroundEl.setAttribute("info-banner", {
-          itemId: selectedItemId,
-        });
-        titleEl.setAttribute("visible", false);
-        cursorEl.setAttribute("position", { x: 0, y: 0, z: -1 });
-        cursorEl.setAttribute("geometry", {
-          radiusInner: 0.03,
-          radiusOuter: 0.04,
-        });
-      } else {
-        //else make the plane invisible
-        fadeBackgroundEl.setAttribute("visible", false);
-        titleEl.setAttribute("visible", true);
-        cursorEl.setAttribute("position", { x: 0, y: 0, z: -3 });
-        cursorEl.setAttribute("geometry", {
-          radiusInner: 0.08,
-          radiusOuter: 0.12,
-        });
-      }
-    });
-  },
+  
 });
